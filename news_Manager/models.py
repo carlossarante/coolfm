8eff6895cacb5fd45d04769a9f493ed9f94e32fa
Carlos <carlos.sarante@conuco.do>
2014-09-25 21:50:43 -0400

    Mejora en vistas de landing page, eficientizacion de codigo. Aun sin serializar.

--- news_Manager/models.py
+++ news_Manager/models.py
@@ -23,11 +23,47 @@ class Post(models.Model):
 
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return '/nouvelles/'+self.slug
+    
     def get_votes(self):
         return self.reads
+    
+    def getLastestNews(self):
+        return Post.objects.all().order_by('-date_posted')        
+    
     def getCategory(self):
         return self.category
+        image_img.short_description = 'Thumb'
+        image_img.allow_tags = True
+
+    def getLastestByCategory(self):
+        categories = Categories.objects.all()
+        news = []
+        for cat in categories:
+            post = cat.post_set.filter(is_published=True).order_by('-date_posted')
+            if post:
+                news.append(post[0])
+        return news
+    
+    
+    def getIndexPageNews(self,page=1):
+        posts = Post.objects.all().order_by('-date_posted')
+        news = self.getLastestByCategory()
+        for p in posts:
+            if p not in news:
+                news.append(p)
+        return news
+
+
+
 
 class Images(models.Model):
     post = models.ForeignKey(Post)
     img = models.ImageField(upload_to='post_images')
+    def img_thumbnail(self):
+        if self.image:
+            return u'<img src="%s" />' % self.image.url_125x125
+        else:
+            return 'No Image'
--- news_Manager/views.py
+++ news_Manager/views.py
@@ -1,115 +1,24 @@
-from django.http import Http404
-from django.core.paginator import Paginator,EmptyPage
-from django.template.defaultfilters import removetags
-from django.db.models import Q
+from django.views.decorators.cache import never_cache
+from django.utils import simplejson as json
 from django.utils.safestring import mark_safe
-from django.http import HttpResponseRedirect,HttpResponse
-from django.shortcuts import render_to_response,RequestContext,render
 from django.template import Library
 from django.template.loader import  render_to_string
-from django.utils import simplejson as json
-from django.views.decorators.cache import never_cache
-from schedules.functions import querysetToArray
+from django.template.defaultfilters import removetags
+from django.shortcuts import render
+from django.http import Http404
+from django.http import HttpResponseRedirect,HttpResponse
+from news_Manager.models import Post
+from django.core.paginator import Paginator,EmptyPage
+
+
 from news_Manager.functions import removeHTML,serialize_post,setLid
 from news_Manager.models import *
 
 
 def newsLoader(request,page_num=1):
-  return render_to_response('index.html',RequestContext(request))
-
-def getPreviewPage(request,id):
-  try:
-    if request.user.is_admin:
-        news = Post.objects.get(id=id)
-        news.content = mark_safe(removetags(news.content,'preview'))
-    return render_to_response('preview/preview.html',{'news':news})
-  except: 
-    raise
-
+  news = Post().getIndexPageNews()
+  return render(request,'test.html',{'news_list':news})
             
-
-def getNewsBySlug(request,id,slug):
-  try:
-    a = request.POST['keyword']
-    try:
-      post = Post.objects.get(id=id)
-      return HttpResponse(json.dumps(serialize_post(post,post.category)),mimetype='application/json')
-    except ValueError:
-      return HttpResponse("Not Available")
-    count(request,post.slug)
-  except:
-    return newsPage(request)
-
-def getMultiplePostPicture(posts):
-  for news in posts:
-    news.picture = querysetToArray(Images.objects.filter(post=news))
-    if not news.picture:
-      news.picture = "/post_images/cool-logo.png"
-  return posts
-
-def getPostPicture(post):
-  post.picture = Images.objects.filter(post=post)
-  return post
-
-def setTemplate(recent_news_list):
-  for i in xrange(0,len(recent_news_list)):
-    if i<2:
-      recent_news_list[i].first=True #If the post belongs to the first column, the class "recientes1" is assigned.
-    else:
-      recent_news_list[i].first=False
-    recent_news_list[i].content=removeHTML(recent_news_list[i].content)
-    getPostPicture(recent_news_list[i])
-  return recent_news_list
-
-def newsPage(request,category=None,pagindex=1):
-  pagindex = int(pagindex)
-  categories = Categories.objects.all()
-  recent_news_list=[]
-  other_news_list = []
-  most_read = Post.objects.filter(is_published=True).order_by('-reads')[:3]  
-  if (category is None):
-    recent_news_list = Post.objects.filter(is_published=True).order_by('-date_posted')[:5]
-    other_news_list = Post.objects.filter(is_published=True).order_by('-date_posted')[6:]
-    pags = Paginator(other_news_list,3)
-    recent_news_list=setLid(recent_news_list)
-    other_news_list=setLid(other_news_list)
-    most_read=setLid(most_read)
-    recent_news_list = setTemplate(recent_news_list)
-    other_news_list = pags.page(pagindex).object_list
-    other_news_list=getMultiplePostPicture(other_news_list)
-    data = []
-    try:
-      a = request.POST['keyword']
-      for r in recent_news_list:
-        data.append(serialize_post(r,r.category)) 
-      for o in other_news_list:
-        data.append(serialize_post(o,o.category))
-      for mr in most_read:
-        data.append(serialize_post(mr,mr.category))
-      data.append({'last':pags.num_pages})
-      return HttpResponse(json.dumps(data),mimetype='application/json')
-    except:
-      return render_to_response('novelles.html',{'recent_news_list':recent_news_list,'other_news_list':other_news_list,'most_read': setLid(most_read),'categories':categories,'page':pagindex,'last_page':pagindex-1,'next_page':pagindex+1,'last':pags.num_pages},RequestContext(request))
-  else:
-    cat = Categories.objects.get(suptag=category)
-    posts = querysetToArray(Post.objects.filter(Q(category=cat),Q(is_published=True)).order_by('-date_posted'))
-    recent_news_list = posts[0]
-    other_news_list = posts[1:]
-    pags = Paginator(other_news_list,3)
-    other_news_list = pags.page(pagindex)
-    other_news_list=getMultiplePostPicture(other_news_list)
-    try:
-      a = request.POST['keyword']
-      data = []    
-      data.append(serialize_post(recent_news_list,recent_news_list.category))
-      for p in other_news_list:
-        data.append(serialize_post(p,p.category))
-      for mr in most_read:
-        data.append(serialize_post(mr,mr.category))
-      data.append({'last':pags.num_pages})
-      return HttpResponse(json.dumps(data),mimetype='application/json')
-    except:
-      return render_to_response('seccion.html',{'recent_news_list':recent_news_list,'other_news_list':other_news_list,'most_read':most_read,'categories':categories,'page':pagindex,'last_page':pagindex-1,'next_page':pagindex+1,'last':pags.num_pages})
 
 def count(request,post_slug):
   try:
