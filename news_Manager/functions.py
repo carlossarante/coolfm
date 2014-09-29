71609633e703cd7681b19c84698d4a48e054683f
Carlos <carlos.sarante@conuco.do>
2014-09-28 21:35:19 -0400

    Reparando error de serializacion

--- news_Manager/functions.py
+++ news_Manager/functions.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from django.utils.html import strip_tags
 from django.template.defaultfilters import removetags
 from django.shortcuts import render,HttpResponse
@@ -11,7 +10,7 @@ import re, HTMLParser
 
 def formatted_render(request,queryset):
   if request.GET.get('format','')=='json':
-    ser_news = PostSerializer(queryset).data #Converted, to json.
+    ser_news = PostSerializer(queryset,many=True).data #Converted, to json.
     return HttpResponse(ser_news,mimetype='application/json')
   else:
     return render(request,'news.html')
