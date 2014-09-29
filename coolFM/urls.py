bee2c81b0454d3fef2ec8c1ab645634501ef592d
Carlos <carlos.sarante@conuco.do>
2014-09-28 20:09:32 -0400

    Modificando urls principales.

--- coolFM/urls.py
+++ coolFM/urls.py
@@ -8,15 +8,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'coolFM.views.index'),
-    url(r'^admin/news_Manager/post/(?P<id>\d+)/preview/$', 'news_Manager.views.getPreviewPage'),
+    #url(r'^admin/news_Manager/post/(?P<id>\d+)/preview/$', 'news_Manager.views.getPreviewPage'),
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^users/', include('user_Manager.urls')),
-    url(r'^novelles/', include('news_Manager.urls')),
+    url(r'^nouvelles/', include('news_Manager.urls')),
     url(r'^schedules/', include('schedules.urls')),
     url(r'^staff/','coolFM.views.load_staff'),
     url(r'^play/','coolFM.views.play'),
     url(r'^mailer/',include('mailer.urls')),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATICFILES_DIRS}),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
-    url(r'^ckeditor/', include('ckeditor.urls')),
+    #url(r'^ckeditor/', include('ckeditor.urls')),
 )
