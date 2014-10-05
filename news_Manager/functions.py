a3eaa736df25c09c97c006292b894ad801d761fb
Carlos <carlos.sarante@conuco.do>
2014-10-05 10:37:35 -0400

    Actualizacion en functions.py

--- news_Manager/functions.py
+++ news_Manager/functions.py
@@ -13,17 +13,14 @@ def formatted_render(request,data):
   if request.GET.get('format','')=='json':
   	return HttpResponse(json.dumps(data),mimetype='application/json')
   else:
-<<<<<<< HEAD
     return render(request,'home.html')
-=======
-    return render(request,'news.html')
 
 def paginationSerializer(request,queryset,page):
 	data = {}
 	post_pages = Paginator(queryset,10)
 	posts = post_pages.page(page)
 	data['count'] = post_pages.num_pages
-	data['nouvelles'] = PostSerializer(posts.object_list).data
+	data['nouvelles'] = PostSerializer(posts.object_list,many=True).data
 	if posts.has_next():
 		data['next'] = posts.next_page_number()
 	else:
@@ -33,4 +30,3 @@ def paginationSerializer(request,queryset,page):
 	else:
 		data['previous'] = None
 	return data
->>>>>>> d71bf4f30f2fa9c482a8be2757bded3236bdbd68
