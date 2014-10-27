c09dd6d5b4a40ca978a22be6f33f56369365ecd9
Carlos <carlos.sarante@conuco.do>
2014-10-26 20:54:14 -0400

    Cambio en serializador de presentadores.

--- user_Manager/serializers.py
+++ user_Manager/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
 from user_Manager.models import Presenter
 class PresenterSerializer(serializers.ModelSerializer):
+	picture = serializers.SerializerMethodField('get_picture_url')
 	class Meta:
 		model = Presenter
+		fields = ('picture','first_name','last_name','bio')
+
+	def get_picture_url(self,obj):
+		return ('/media/%s'%obj.img.name)
