cc1556855c1374fe11f1802520e313ec0ef0c42d
Osiris Peña <osiris.pena@conuco.do>
2014-11-01 15:49:00 -0400

    Conuco Crop Tools version 1.2

--- news_Manager/widgets.py
+++ news_Manager/widgets.py
@@ -32,13 +32,12 @@ class WYMEditor(forms.Textarea):
 class ImageCropper(widgets.TextInput):
     def render(self,name,value,attrs=None):
         return mark_safe('''
-                            <div  ng-controller="Ctrl">
-                                <input type="file" onchange="angular.element(this).scope().handleFileSelect(event)" />
-                                <div class="cropArea">
-                                    <img-crop image="myImage" result-image="myCroppedImage" area-type="square" as-ratio-x="1.77" as-ratio-y="1"></img-crop>
+                            <div  ng-controller="ImgCtrl">
+                                <input type="file" onchange="angular.element(this).scope().handleFileSelect(event)" ng-hide="croped || selected"/>
+                                <button type="button" ng-click="cropImg()" ng-hide="croped || !selected">CROP!</button>
+                                <div class="cropArea" ng-hide="croped || !selected">
+                                    <img-crop image="myImage" area-min-size="50" result-image-url="myCroppedImage" area-type="square" as-ratio-x="1.77" as-ratio-y="1"></img-crop>
                                 </div>
-                                <div>Cropped Image:</div>
-                                <div><img src="{{myCroppedImage}}" /></div>
-                                <input type="url" value="{{myCroppedImage}}">
+                                <div ng-show="croped" style="width:200px;"><img src="{{myCroppedImage}}" /></div>
                             </div>
                         ''')
--- static/js/admin/app.js
+++ static/js/admin/app.js
@@ -1,17 +1,44 @@
 angular.module('app', ['ngImgCrop'])
-  .controller('Ctrl', function($scope) {
+  .controller('FormCtrl',['$scope',function($scope){
+      $scope.formData = new FormData();
+      $scope.appendImg = function (scope, file){
+        $scope.formData.append("img"+scope.$id,file);
+      }
+      $scope.sendForm = function () {
+        var request = new XMLHttpRequest();
+        request.open("POST", "/static/");
+        request.send($scope.formData);
+      };
+
+    }]) 
+  .controller('ImgCtrl', ['$scope',function($scope) {
     $scope.myImage='';
     $scope.myCroppedImage='';
+    $scope.file ={};
+    $scope.selected=false;
+    $scope.croped = false;
+
+    console.log($scope.$id)
+    console.log($scope)
 
     $scope.handleFileSelect = function (evt) {
       var file=evt.currentTarget.files[0];
       var reader = new FileReader();
+
+      $scope.selected=true;
+
       reader.onload = function (evt) {
         $scope.$apply(function($scope){
           $scope.myImage=evt.target.result;
         });
       };
       reader.readAsDataURL(file);
+      //$scope.file = myImage;
     };
+    $scope.cropImg = function (){
+      $scope.croped = true;
+      $scope.appendImg($scope,$scope.file);
+    }
     //angular.element(document.querySelector('#fileInput')).on('change',$scope.handleFileSelect);
-  });
+
+  }]);
