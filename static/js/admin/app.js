339cbc6483363c459e51e9017f2b1d053e68cadb
Osiris <osiris.pena@conuco.do>
2014-11-05 22:29:07 -0400

    Generando Blob de recorte de imagen

--- static/js/admin/app.js
+++ static/js/admin/app.js
@@ -33,12 +33,36 @@ angular.module('app', ['ngImgCrop'])
         });
       };
       reader.readAsDataURL(file);
-      //$scope.file = myImage;
+      $scope.img=$scope.dataURItoBlob($scope.myCroppedImage);
+      console.log($scope.img);
     };
     $scope.cropImg = function (){
       $scope.croped = true;
-      $scope.appendImg($scope,$scope.file);
-    }
+      $scope.appendImg($scope,$scope.img);
+    };
     //angular.element(document.querySelector('#fileInput')).on('change',$scope.handleFileSelect);
 
+    $scope.dataURItoBlob = function(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs
+      var byteString = atob(dataURI.split(',')[1]);
+     /* var byteString;
+      if (dataURI.split(',')[0].indexOf('base64') >= 0)
+          byteString = atob(dataURI.split(',')[1]);
+      else
+          byteString = unescape(dataURI.split(',')[1]);*/
+
+      // separate out the mime component
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+      var ia = new Uint8Array(ab);
+      for (var i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+      }
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab],{"type":mimeString});
+      return blob
+    };
   }]);
